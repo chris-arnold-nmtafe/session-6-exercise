d58a67851d0097b974fd992298064353653241eb
Chris Arnold <chris.arnold@nmtafe.wa.edu.au>
2025-09-03 11:01:45 +0800

    Update pwdgen.py

--- pwdgen.py
+++ pwdgen.py
@@ -1,14 +1,14 @@
 import sys
 import random
 
-number_chars=10
-if len(sys.argv)>1:
-    number_chars=int(sys.argv[1])
+number_chars = 10
+if len(sys.argv) > 1:
+    number_chars = int(sys.argv[1])
 password=""
 for index in range(number_chars):
     # Allow us to generate some  
     # capital letters as well.
-    character_index=random.randint(0,52)
-    character_index+=ord('b')
-    password+=chr(character_index)
+    character_index = random.randint(0,52)
+    character_index += ord('b')
+    password += chr(character_index)
 print(password)
